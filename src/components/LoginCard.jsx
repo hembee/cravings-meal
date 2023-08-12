149413e954689bdfee7e29cc15b76549b185219d
hembee <hembee999@gmail.com>
2023-08-12 08:42:06 +0100

    link to home

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -1,14 +1,34 @@
-import styles from "../styles/LoginCard.module.css"
+import { useState } from "react";
+import styles from "../styles/LoginCard.module.css";
 // import axios from "axios"
 
 const LoginCard = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className={styles.loginCard}>
-          <form action="/" method="post">
-              
+      <form action="/" method="post">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginCard
+export default LoginCard;
