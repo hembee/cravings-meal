8d529256442958c11bcd4ddea10876e3d804e276
Dark_Tony <Hembee999@gmail.com>
2023-12-10 20:11:09 +0100

    cart finishes

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -2,30 +2,32 @@ import styles from "../styles/Food.module.css";
 import burger from "../images/burger.png";
 import donut from "../images/donuts.png";
 import pasta from "../images/pasta.png";
+import { Link } from "react-router-dom";
+
 const Food = () => {
-    const meals = [
-      {
-        id: 1,
-        img: burger,
-        header: "Burger",
-        text: "Lorem ipsum dolor sit amet cocon adipisicing elit........",
-        price: "₦2400",
-      },
-      {
-        id: 2,
-        img: pasta,
-        header: "Pasta",
-        text: "Lorem ipsum dolor sit amet cocon adipisicing elit........",
-        price: "₦1800",
-      },
-      {
-        id: 3,
-        img: donut,
-        header: "Donut",
-        text: "Lorem ipsum dolor sit amet cocon adipisicing elit........",
-        price: "₦800",
-      },
-    ];
+  const meals = [
+    {
+      id: 1,
+      img: burger,
+      header: "Burger",
+      text: "Lorem ipsum dolor sit amet cocon adipisicing elit........",
+      price: "₦2400",
+    },
+    {
+      id: 2,
+      img: pasta,
+      header: "Pasta",
+      text: "Lorem ipsum dolor sit amet cocon adipisicing elit........",
+      price: "₦1800",
+    },
+    {
+      id: 3,
+      img: donut,
+      header: "Donut",
+      text: "Lorem ipsum dolor sit amet cocon adipisicing elit........",
+      price: "₦800",
+    },
+  ];
   return (
     <section className={styles.food}>
       {meals.map((meal) => {
@@ -43,8 +45,10 @@ const Food = () => {
               <p>{meal.text}</p>
             </div>
             <div className={styles.col3}>
-            <h3>{meal.price}</h3>
-              <button className={styles.increaseBtn}>+</button>
+              <h3>{meal.price}</h3>
+              <Link to={"/cart"} className={styles.increaseBtn}>
+                +
+              </Link>
             </div>
           </div>
         );
