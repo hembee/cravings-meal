5fbb2c225c10a269d90b7f8bf1305ef736bcc089
Dark_Tony <Hembee999@gmail.com>
2023-12-29 20:53:42 +0100

    modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,7 @@
 import styles from "../styles/Modal.module.css";
 import check from "../images/check.png";
+import { Link } from "react-router-dom";
+
 export const Modal = ({ modalHandler }) => {
   return (
     <div className={styles.blur} onClick={modalHandler}>
@@ -11,9 +13,9 @@ export const Modal = ({ modalHandler }) => {
       >
         <img className={styles.img} src={check} />
         <h1 className={styles.text}>Order confirmed</h1>
-        <button className={styles.submitBtn} onClick={modalHandler}>
+        <Link to={"/"} className={styles.submitBtn} onClick={modalHandler}>
           OK
-        </button>
+        </Link>
       </div>
     </div>
   );
